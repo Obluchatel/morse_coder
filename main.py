50ff96293134631b04175296cc6eb0c736cd9b5d
Obluchatel <azamatovartem@gmail.com>
2021-01-09 11:56:57 +0100

    Deleted comment

--- main.py
+++ main.py
@@ -13,31 +13,3 @@ for letters in range(len(splited)):
     output = ''.join(a)
 print(output)
 
-
-# def morze_coder(word):
-#     global output
-#     splited = list(word)
-#     print(splited)
-#     while len(splited) != 0:
-#         searched = alphabet.index(splited[0])
-#         splited[searched] = morze_alphabet[searched]
-#         print(splited[searched])
-#         output = splited[searched]
-#         del splited[0]
-#     print(output)
-# output= []
-#
-# def morze_coder(word):
-#     global output
-#     splited = list(word)
-#     print(splited)
-#     for letters in range(len(splited)):
-#         searched = alphabet.index(splited[0])
-#         splited[searched] = morze_alphabet[searched]
-#         print(splited[searched])
-#         output.append(splited[searched])
-#         del (splited[searched])
-#         print(output)
-#     print(output)
-#
-#
