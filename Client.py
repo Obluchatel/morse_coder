150b9b18c4d362b5454dfe72b99368bdaa2d6894
Obluchatel <azamatovartem@gmail.com>
2021-04-24 11:23:45 +0200

    Server and Client fixed

--- Client.py
+++ Client.py
@@ -1,4 +1,7 @@
 import socket
+import winsound
+import time
+
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 8888  # The port used by the server
@@ -12,3 +15,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.sendall(message.encode())
         data = s.recv(1024)
         print('Received' + (data.decode()))
+        sound = str(data.decode())
+        print(sound)
+        for i in data.decode():
+            print(i)
+            if i == '.':
+                winsound.Beep(1000, 100)  # Beep at 1000 Hz for 100 ms
+                time.sleep(0.1)
+            elif i == '-':
+                winsound.Beep(1000, 600)  # Beep at 1000 Hz for 100 ms
+                time.sleep(0.1)
+            elif i == ' ':
+                pass
+
+        # winsound.Beep(1000, 100)  # Beep at 1000 Hz for 100 ms
+        # winsound.Beep(1000, 400)  # Beep at 1000 Hz for 100 ms
+
+
+
+
